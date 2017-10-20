7f182305ea1032a3acacb3873ae52d52de2fb936
Matt Joiner <anacrolix@gmail.com>
2017-10-20 12:46:12 +1100

    Add tests for http.Request.Context.Done in various circumstances

---
+++ http_test.go
@@ -0,0 +1,76 @@
+package wat
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"golang.org/x/net/websocket"
+
+	"github.com/anacrolix/missinggo/httptoo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHttpServerHandlerRequestContextDone(t *testing.T) {
+	handlerRunning := make(chan struct{})
+	var handlerDone <-chan struct{}
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handlerDone = r.Context().Done()
+		select {
+		case <-r.Context().Done():
+			t.Fatal("request done as soon as handler started")
+		default:
+		}
+		close(handlerRunning)
+	write:
+		for {
+			select {
+			case <-r.Context().Done():
+				break write
+			default:
+				_, err := w.Write(make([]byte, 1024))
+				if err != nil {
+					break write
+				}
+			}
+		}
+	}))
+	r, err := http.Get(s.URL)
+	require.NoError(t, err)
+	<-handlerRunning
+	assert.NoError(t, r.Body.Close())
+	<-handlerDone
+}
+
+func TestWebSocketRequestContextDone(t *testing.T) {
+	serverHasConn := make(chan struct{})
+	var handlerDone <-chan struct{}
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handlerDone = r.Context().Done()
+		websocket.Handler(func(ws *websocket.Conn) {
+			select {
+			case <-r.Context().Done():
+				t.Fatal("request done as soon as server got conn")
+			default:
+			}
+			close(serverHasConn)
+			_, err := ws.Read(nil)
+			assert.Equal(t, io.EOF, err)
+			// Expect this to close when the websocket is Closed.
+			<-r.Context().Done()
+		}).ServeHTTP(w, r)
+	}))
+	u, err := url.Parse(s.URL)
+	require.NoError(t, err)
+	u = httptoo.AppendURL(u, &url.URL{
+		Scheme: "ws",
+	})
+	ws, err := websocket.Dial(u.String(), "", s.URL)
+	require.NoError(t, err)
+	<-serverHasConn
+	assert.NoError(t, ws.Close())
+	<-handlerDone
+}
